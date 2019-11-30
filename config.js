08c0e6b407fb9c3e037fee59eaa7c57b866c15c7
happytail <vitaly.mortikov@mail.ru>
2019-11-30 08:47:56 +0300

    d1: interface scratch

---
+++ config.js
@@ -0,0 +1,18 @@
+var cfg = {
+    'dispSize': {
+        'x': 640,
+        'y': 480,
+    },
+    'bricksCnt': {
+        'row': 14,
+        'col': 3,
+    },
+    'bricksSize': {
+        'x': 30,
+        'y': 90,
+    },
+    'bricksSpace': {},
+};
+
+cfg['bricksSpace']['x'] = 45;
+cfg['bricksSpace']['y'] = 105;
